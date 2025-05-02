36daa0058fe17958ffc8f7e981d36a7ab825ae35
sshyuny <thgustls@gmail.com>
2025-05-02 17:26:57 +0900

    feat: JWT 적용

--- src/main/java/com/sshyu/ZibNote/adapter/in/web/member/MemberController.java
+++ src/main/java/com/sshyu/ZibNote/adapter/in/web/member/MemberController.java
@@ -4,11 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sshyu.zibnote.adapter.in.web.common.ApiResponse;
 import com.sshyu.zibnote.adapter.in.web.member.dto.LoginReqDto;
+import com.sshyu.zibnote.domain.auth.model.Token;
 import com.sshyu.zibnote.domain.auth.port.in.AuthUseCase;
 import com.sshyu.zibnote.domain.member.model.Member;
 import com.sshyu.zibnote.domain.member.port.in.MemberUseCase;
@@ -19,30 +19,32 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
 @RestController
 public class MemberController {
 
     private final MemberUseCase memberUseCase;
     private final AuthUseCase authUseCase;
     
-    @PostMapping("/login")
+    @PostMapping("/pass/api/login")
     public ResponseEntity<ApiResponse<Void>> login(HttpSession httpSession, @RequestBody LoginReqDto loginReqDto) {
 
-        authUseCase.login(loginReqDto.getName());
+        Token token = authUseCase.login(loginReqDto.getName());
         log.info("로그인 성공! " + loginReqDto.getName());
 
-        return ResponseEntity.ok(ApiResponse.codeAndMessage("login success", "로그인 성공!"));
+        return ResponseEntity
+                    .ok()
+                    .header("Authorization", "Bearer " + token.getToken())
+                    .body(ApiResponse.codeAndMessage("login success", "로그인 성공!"));
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/api/logout")
     public ResponseEntity<ApiResponse<Void>> logout(@RequestBody LoginReqDto loginReqDto) {
 
         authUseCase.logout();
         return ResponseEntity.ok(ApiResponse.codeAndMessage("logout success", "로그아웃 성공!"));
     }
 
-    @PostMapping("/register")
+    @PostMapping("/pass/api/register")
     public ResponseEntity<ApiResponse<String>> post(@RequestBody LoginReqDto loginReqDto) {
 
         memberUseCase.register(Member.builder().name(loginReqDto.getName()).build());
@@ -51,7 +53,7 @@ public class MemberController {
         return ResponseEntity.ok(ApiResponse.successWithDataAndMessage(loginReqDto.getName(), "회원가입 성공!"));
     }
 
-    @GetMapping
+    @GetMapping("/api/member")
     public ResponseEntity<ApiResponse<String>> get() {
 
         String memberName = memberUseCase.getMember(authUseCase.getMemberId()).getName();
