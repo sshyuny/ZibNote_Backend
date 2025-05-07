0dabb6961681f700ace174f7e4a88a0278130f98
sshyuny <thgustls@gmail.com>
2025-05-07 14:01:20 +0900

    refactor: CORS 설정을 필터 수동 등록 방식으로 변경

--- src/main/java/com/sshyu/ZibNote/configure/filter/PassApiFilter.java
+++ src/main/java/com/sshyu/ZibNote/configure/filter/PassApiFilter.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.sshyu.zibnote.application.service.auth.JwtUtil;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +11,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class JwtAuthFilter extends OncePerRequestFilter {
+public class PassApiFilter extends OncePerRequestFilter  {
     
-    private final JwtUtil jwtUtil;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
@@ -32,24 +28,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
 
-        String authHeader = request.getHeader("Authorization");
-
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 없습니다.");
-            return;
-        }
-        
-        String token = authHeader.substring(7);
-
-        if (!jwtUtil.validateToken(token)) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "잘못된 접근입니다.");
-            return;
-        }
-        
-        Long memberId = jwtUtil.extractMemberId(token);
-        request.setAttribute("memberId", memberId);
-
         filterChain.doFilter(request, response);
     }
-
 }
--- src/main/java/com/sshyu/ZibNote/configure/web/WebConfig.java
+++ src/main/java/com/sshyu/ZibNote/configure/web/WebConfig.java
@@ -3,11 +3,15 @@ package com.sshyu.zibnote.configure.web;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.sshyu.zibnote.application.service.auth.JwtUtil;
 import com.sshyu.zibnote.configure.filter.JwtAuthFilter;
+import com.sshyu.zibnote.configure.filter.PassApiFilter;
 
 
 @Configuration
@@ -17,21 +21,25 @@ public class WebConfig implements WebMvcConfigurer {
     public JwtAuthFilter jwtAuthFilter(JwtUtil jwtUtil) {
         return new JwtAuthFilter(jwtUtil);
     }
+    @Bean
+    public PassApiFilter passApiFilter() {
+        return new PassApiFilter();
+    }
 
     @Bean
-    public FilterRegistrationBean<JwtAuthFilter> jwtFilter(JwtUtil jwtUtil) {
+    public FilterRegistrationBean<JwtAuthFilter> jwtAuthFilterRegister(JwtUtil jwtUtil) {
         FilterRegistrationBean<JwtAuthFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(jwtAuthFilter(jwtUtil));
         registrationBean.addUrlPatterns("/api/*");
         return registrationBean;
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true); // 세션/쿠키 사용할 경우
+    @Bean
+    public FilterRegistrationBean<PassApiFilter> passFilterRegister() {
+        FilterRegistrationBean<PassApiFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(passApiFilter());
+        registrationBean.addUrlPatterns("/pass/api/*");
+        return registrationBean;
     }
     
 }
