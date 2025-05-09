419a3ff80aa8df1c3b295c4ab7a0c894e9cc7969
sshyuny <thgustls@gmail.com>
2025-05-09 11:29:42 +0900

    refactor: CORS 설정을 CorsRegistry 등록 방식으로 변경

--- src/main/java/com/sshyu/ZibNote/configure/web/WebConfig.java
+++ src/main/java/com/sshyu/ZibNote/configure/web/WebConfig.java
@@ -1,42 +1,33 @@
 package com.sshyu.zibnote.configure.web;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.sshyu.zibnote.application.service.auth.JwtUtil;
-import com.sshyu.zibnote.configure.filter.JwtAuthFilter;
-import com.sshyu.zibnote.configure.filter.CustomCorsFilter;
+import com.sshyu.zibnote.configure.interceptor.LoginCheckInterceptor;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public JwtAuthFilter jwtAuthFilter(JwtUtil jwtUtil) {
-        return new JwtAuthFilter(jwtUtil);
-    }
-    @Bean
-    public CustomCorsFilter customCorsFilter() {
-        return new CustomCorsFilter();
-    }
+    @Autowired
+    LoginCheckInterceptor loginCheckInterceptor;
 
-    @Bean
-    public FilterRegistrationBean<JwtAuthFilter> jwtAuthFilterRegister(JwtUtil jwtUtil) {
-        FilterRegistrationBean<JwtAuthFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthFilter(jwtUtil));
-        registrationBean.addUrlPatterns("/api/*");
-        return registrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/pass/api/**");
     }
-
-    @Bean
-    public FilterRegistrationBean<CustomCorsFilter> customCorsFilterRegister() {
-        FilterRegistrationBean<CustomCorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(customCorsFilter());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(0);
-        return registrationBean;
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 세션/쿠키 사용할 경우
     }
     
 }
