6e8e1a5ffee4e45ffa2949074dd1a67b7e5e7613
sshyuny <thgustls@gmail.com>
2025-05-09 11:29:42 +0900

    refactor: CORS 설정을 CorsRegistry 등록 방식으로 변경

--- src/main/java/com/sshyu/ZibNote/configure/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/sshyu/ZibNote/configure/interceptor/LoginCheckInterceptor.java
@@ -1,45 +1,45 @@
-package com.sshyu.zibnote.configure.filter;
+package com.sshyu.zibnote.configure.interceptor;
 
-import java.io.IOException;
-
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.sshyu.zibnote.application.service.auth.JwtUtil;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor
-public class JwtAuthFilter extends OncePerRequestFilter {
-    
-    private final JwtUtil jwtUtil;
+public class LoginCheckInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private final JwtUtil jwtUtil;
+    
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return true; // 무조건 통과
+        }
 
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 없습니다.");
-            return;
+            return false;
         }
         
         String token = authHeader.substring(7);
 
         if (!jwtUtil.validateToken(token)) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "잘못된 접근입니다.");
-            return;
+            return false;
         }
         
         Long memberId = jwtUtil.extractMemberId(token);
         request.setAttribute("memberId", memberId);
-
-        filterChain.doFilter(request, response);
+        return true;
     }
-
 }
